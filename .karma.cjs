70093a4ba8c11f4b48846922686a15ee85cbb06b
cupcakearmy <hi@nicco.io>
2022-10-18 14:08:27 +0200

    use headless

--- .karma.cjs
+++ .karma.cjs
@@ -11,7 +11,7 @@ module.exports = function (config) {
       { pattern: 'test/**/*.js', type: 'module' },
     ],
 
-    browsers: ['Chromium', 'Firefox', 'WebKit'],
+    browsers: ['ChromiumHeadless', 'FirefoxHeadless', 'WebKitHeadless'],
 
     preprocessors: {},
     reporters: ['progress'],
