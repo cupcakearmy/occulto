dbc5bf97d90bb9e080411cd9775c70d6b02fcc9b
cupcakearmy <hi@nicco.io>
2022-10-18 15:42:55 +0200

    maybe this time

--- rename.sh
+++ rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Rename .js files to .cjs
-rename -S .js .cjs dist/cjs/**
-sed -i '' 's/\.js"/\.cjs"/g' dist/cjs/index.cjs
+rename -S .js .cjs dist/cjs/**/*
+sed -i '' 's/\.js"/\.cjs"/g' dist/cjs/**/*.cjs
